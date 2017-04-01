38475bb85665e7b818039e45c3bb40e4ff91bf84
吳育騫 <andrew90622@gmail.com>
2017-04-01 17:06:09 +0800

    Test ecc version

--- UserStudy1.js
+++ UserStudy1.js
@@ -8,7 +8,7 @@ var UserStudy1 = function(experiment) {
 	this.nextSentenceNum = 0 + this.offset;
 	this.measuredAlready = false;
 	this.count = 0;
-	this.questionSet = ["What is your blood type ?","Where do you come from ?","What year are you in ?","Name three fruits you like to eat ?","Are you married or not ?","Who is your favorite movie star ?"]
+	this.questionSet = ["幹 ?","Where do you come from ?","What year are you in ?","Name three fruits you like to eat ?","Are you married or not ?","Who is your favorite movie star ?"]
 	this.answerSet = ["Sophia, Emma, Olivia","it was held in Los Angeles","born in October 28th, 1955","Georgia Aquarium in Atlanta","Mandarin, English, Spanish"]
 	this.randomNum = [1,2,3,4,5]
 	this.nowUse = []
@@ -45,17 +45,8 @@ var UserStudy1 = function(experiment) {
 		this.experiment.hideCharacter();
 		var string = this.nowUse[this.nextSentenceNum];
 		string = string.split(" ");
-		if(this.count == 0){
-			for(var i = 0; i < string.length; i++){
-				this.experiment.showCharacter(string[i], (9 * string.length / 2 - 9 * i) % 360, this.EccNum, 1);
-			}
-			this.count ++;
-		}
-		else if(this.count == 1){
-			for(var i = 0; i < string.length; i++){
-				this.experiment.showCharacter(string[i], (6 * string.length / 2 - 6 * i) % 360, this.EccNum, 1);
-			}
-			this.count = 0;
+		for(var i = 0; i < string.length; i++){
+			this.experiment.showCharacter(string[i], (9 * string.length / 2 - 9 * i) % 360, this.EccNum, 1);
 		}
 	}
 	this.displayOnLeft = function() {
@@ -64,18 +55,10 @@ var UserStudy1 = function(experiment) {
 		this.experiment.hideCharacter();
 		var string = this.nowUse[this.nextSentenceNum];
 		string = string.split(" ");
-		if(this.count == 0){		
-			for(var i = string.length - 1 ; i >= 0; i--){
-				this.experiment.showCharacter(string[string.length - 1 - i], (180 + 9 * string.length / 2 - 9 * i) % 360, this.EccNum, 1);
-			}
-			this.count++;
+		for(var i = string.length - 1 ; i >= 0; i--){
+			this.experiment.showCharacter(string[string.length - 1 - i], (180 + 9 * string.length / 2 - 9 * i) % 360, this.EccNum, 1);
 		}
-		else if(this.count ==1){
-			for(var i = string.length - 1 ; i >= 0; i--){
-				this.experiment.showCharacter(string[string.length - 1 - i], (180 + 6 * string.length / 2 - 6 * i) % 360, this.EccNum, 1);
-			}
-			this.count = 0;
-		}
+
 	}
 	this.displayOnBottom = function(){
 		this.EccNum = document.getElementById("eccNumber").value;
